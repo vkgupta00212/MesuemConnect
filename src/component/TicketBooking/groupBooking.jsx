4c93da1c4e02863141617c6705f89bb1a8e37ed1
Vishal Kumar <149326640+vkgupta00212@users.noreply.github.com>
2024-12-11 15:25:05 +0530

    Update groupBooking.jsx

--- src/component/TicketBooking/groupBooking.jsx
+++ src/component/TicketBooking/groupBooking.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FiArrowRight } from "react-icons/fi";
-import AboutBackground from "../Assets/about-background.png";
+import AboutBackground from "../assets/about-background.png";
 import { useNavigate } from "react-router-dom";
 import "./groupBooking.css";
 
